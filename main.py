a4b3df903a2f99149ace23e60657868d25222ac7
MrDomian <41548647+MrDomian@users.noreply.github.com>
2023-07-10 01:33:53 +0200

    Adding new prototype functions

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import datetime
 import numpy as np
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
+# import seaborn as sns
+# import matplotlib.pyplot as plt
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
