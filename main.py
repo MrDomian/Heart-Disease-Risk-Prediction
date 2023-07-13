47dcbb2fc956d3c365a4336ab9d8f6e848c48c34
mrdomian <mrdomian@gmail.com>
2023-07-14 01:07:26 +0200

    Add new functions for machine learning models

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ import matplotlib.pyplot as plt
 
 from itertools import combinations_with_replacement
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.metrics import accuracy_score, precision_score, f1_score, roc_auc_score, recall_score, confusion_matrix
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder, PolynomialFeatures, StandardScaler, MinMaxScaler, \
     KBinsDiscretizer
+from imblearn.combine import SMOTETomek
+from imblearn.over_sampling import SMOTE
 
 
-def display_results(model, X_train, X_test, y_train, y_test):
+def train_model(model, X_train, X_test, y_train, y_test):
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
 
@@ -20,33 +22,76 @@ def display_results(model, X_train, X_test, y_train, y_test):
     f1 = f1_score(y_test, y_pred)
     sensitivity = recall_score(y_test, y_pred)
     precision = precision_score(y_test, y_pred)
-    cm = confusion_matrix(y_test, y_pred)
-    tn, fp, fn, tp = cm.ravel()
+    conf_matrix = confusion_matrix(y_test, y_pred)
+    tn, fp, fn, tp = conf_matrix.ravel()
     g_mean = (tp / (tp + fn)) * (tn / (tn + fp))
     specificity = tn / (tn + fp)
+    avg = (f1 + roc_auc + g_mean) / 3
 
-    print("Accuracy:", round(accuracy, 2))
-    print("ROC AUC Score:", round(roc_auc, 2))
-    print("G-Mean:", round(g_mean, 2))
-    print("F1 Score:", round(f1, 2))
-    print("Sensitivity:", round(sensitivity, 2))
-    print("Specificity:", round(specificity, 2))
-    print("Precision:", round(precision, 2))
+    list_results = [round(result, 2) for result in
+                    [accuracy, roc_auc, g_mean, f1, sensitivity, specificity, precision, avg, tn, fp, fn, tp]]
 
+    return list_results, conf_matrix
+
+
+def display_results(results):
+    accuracy = results[0]
+    roc_auc = results[1]
+    g_mean = results[2]
+    f1 = results[3]
+    sensitivity = results[4]
+    specificity = results[5]
+    precision = results[6]
+    avg = results[7]
+
+    print("Accuracy:", accuracy)
+    print("ROC AUC Score:", roc_auc)
+    print("G-Mean:", g_mean)
+    print("F1 Score:", f1)
+    print("Sensitivity:", sensitivity)
+    print("Specificity:", specificity)
+    print("Precision:", precision)
+    print("AVG:", avg)
+
+
+def display_confusion_matrix(conf_matrix):
     plt.figure(figsize=(8, 6))
-    sns.heatmap(cm, annot=True, cmap="PuBuGn", fmt="d")
+    sns.heatmap(conf_matrix, annot=True, cmap="PuBuGn", fmt="d")
     plt.xlabel("Predicted")
     plt.ylabel("True")
     plt.title("Confusion Matrix")
     plt.show()
 
 
-# Feature engineering
+def balance_data_smote(X, y):
+    X_bal, y_bal = SMOTE().fit_resample(X, y)
+    return X_bal, y_bal
 
+
+def balance_data_smotetomek(X, y):
+    zeros = np.count_nonzero(y.values == 0)
+    X_bal, y_bal = SMOTETomek(sampling_strategy ={0: zeros, 1: int(zeros/2)}).fit_resample(X, y)
+    return X_bal, y_bal
+
+
+# # Test functions: train_model, display_results, display_confusion_matrix
+# data = pd.read_csv('heart_disease_risk.csv')
+# X = data.drop('decision', axis=1)
+# y = data['decision']
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+# model = LogisticRegression(solver='liblinear')
+#
+# results, conf_matrix = train_model(model, X_train, X_test, y_train, y_test)
+# display_results(results)
+# display_confusion_matrix(conf_matrix)
+
+
+# Feature engineering
 def one_hot_encoding(data, categorical_columns):
     encoder = OneHotEncoder()
     encoded_data = pd.DataFrame(encoder.fit_transform(data[categorical_columns]).toarray())
-    encoded_data.columns = [f"{col}_{cat}" for col in categorical_columns for cat in encoder.categories_[categorical_columns.index(col)]]
+    encoded_data.columns = [f"{col}_{cat}" for col in categorical_columns for cat in
+                            encoder.categories_[categorical_columns.index(col)]]
     data = pd.concat([data.reset_index(drop=True), encoded_data], axis=1)
     data.drop(categorical_columns, axis=1, inplace=True)
     return data
@@ -91,7 +136,7 @@ def create_polynomial_features(data, degree):
 def create_interaction_features(data):
     interaction_data = pd.DataFrame(index=data.index)
     for i in range(len(data.columns)):
-        for j in range(i+1, len(data.columns)):
+        for j in range(i + 1, len(data.columns)):
             interaction_data[data.columns[i] + '*' + data.columns[j]] = data.iloc[:, i] * data.iloc[:, j]
     data = pd.concat([data, interaction_data], axis=1)
     return data
