19dc7b590d99af4c8090dacd1f618b8edd113fa4
MrDomian <41548647+MrDomian@users.noreply.github.com>
2023-07-19 01:59:06 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,6 @@ import streamlit as st
 
 
 st.set_page_config(page_title="Disease Prediction", page_icon="+")
-hide_st_style = """
-<style>
-    footer {visibility: hidden;}
-</style>
-"""
-st.markdown(hide_st_style, unsafe_allow_html=True)
 
 model = pickle.load(open('model.sav', 'rb'))
 
